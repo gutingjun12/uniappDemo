52bcf621c395df0af39ceb5e0a1882e46525138c
gutingjun12 <714979470@qq.com>
2020-06-11 17:31:35 +0800

    eggjs

--- egg/app/router.js
+++ egg/app/router.js
@@ -7,6 +7,5 @@ module.exports = app => {
 	const { router, controller } = app;
 	router.get('/', controller.home.hello);
 	router.get('/getList', controller.user.list);
-	router.get('/cook', controller.cook.list);
 
 };
--- egg/app/service/user.js
+++ egg/app/service/user.js
@@ -5,8 +5,11 @@ const Service = require('egg').Service;
 class UserService extends Service {
   async getUser() {
 		return await this.ctx.model.User.find({});
-
   }
+	
+	async addUser(obj) {
+		
+	}
 
 
 }
